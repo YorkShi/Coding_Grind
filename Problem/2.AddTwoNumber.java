820528269519adf1c027e03cc72b934c1b012ce5
ShingJan <yuanj.shi@hotmail.com>
2016-02-28 21:34:41 -0500

    Common Problems: null pointer time limit exceeded

--- Problem/2.AddTwoNumber.java
+++ Problem/2.AddTwoNumber.java
@@ -16,6 +16,48 @@
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        	
+        
+        ListNode x = new ListNode(0);
+        ListNode before = x;
+        ListNode head = x;
+        
+        head.next = x;
+        
+        while(null != l1 && null != l2 ){
+            
+            x.val = l1.val + l2.val;
+            
+            x.next = new ListNode(x.val/10);
+            x.val = x.val%10;
+            
+            before = x;
+            x = x.next;
+            
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        
+        ListNode rest;
+        
+        if(l1 == null){
+            rest = l2;
+        }else{
+            rest = l1;
+        }
+        
+        while(rest != null){
+            x.val = rest.val;
+            
+            x.next = new ListNode(x.val/10);
+            x.val = x.val%10;
+            before = x;
+            x = x.next;
+        }
+        
+        if(before.next != null && before.next.val == 0){
+            before.next = null;
+        }
+        
+        return head;
     }
 }
