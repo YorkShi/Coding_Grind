43a021466841d1b1dc0c26c64b6d3b6085a0fa80
ShingJan <yuanj.shi@hotmail.com>
2016-04-30 20:25:59 -0400

    little changes

--- Leetcode/src/medianSortedArray.java
+++ Leetcode/src/medianSortedArray.java
@@ -2,15 +2,15 @@
  * Created by Isaac on 4/30/16.
  */
 public class medianSortedArray {
-    
-    public int medianSortedArray(){
+
+    public double medianSortedArray(int[] nums1, int[] nums2){
 
     }
 
 
 
     public static void main(String[] args) {
-        LongestSubstring slt = new LongestSubstring();
+        medianSortedArray slt = new medianSortedArray();
         int len = slt.lengthOfLongestSubstring("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybl"
                 + "dbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco");
         System.out.println(len);
