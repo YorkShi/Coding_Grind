d45ab6fe66779df28db9f89662b359bfbeb68c4e
ShingJan <yuanj.shi@hotmail.com>
2016-04-30 20:23:04 -0400

    Median sorted arrays

---
+++ Leetcode/src/medianSortedArray.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Isaac on 4/30/16.
+ */
+public class medianSortedArray {
+    
+    public int medianSortedArray(){
+
+    }
+
+
+
+    public static void main(String[] args) {
+        LongestSubstring slt = new LongestSubstring();
+        int len = slt.lengthOfLongestSubstring("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybl"
+                + "dbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco");
+        System.out.println(len);
+    }
+}
