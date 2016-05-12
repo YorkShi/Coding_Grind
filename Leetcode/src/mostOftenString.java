5083a38a78fb5bf0020a133f3757bede3f4668cf
ShingJan <yuanj.shi@hotmail.com>
2016-05-11 21:20:58 -0400

    Most Often String

---
+++ Leetcode/src/mostOftenString.java
@@ -0,0 +1,19 @@
+／* 在一个长度为N的string中，找到出现次数最多的substring出现的次数。 substring限制如下：
+1. 长度在K和L之间， 2 <= k < L <= 26
+2. substring当中包含的unique的字符个数不超过M个 2 <= M <= 26
+
+Test case
+Input:
+k = 2
+L = 4
+M = 26
+ababab
+
+Output:
+3
+
+Explain:
+for size 2 substring, ab repeats 3 times
+for size 3 substring, aba repeats 2 times and bab repeats twice
+for size 4 substring, no substring repeats
+*/
