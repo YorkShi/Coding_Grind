00ff14450fe6fc8060e659170bc9f444a95b144b
ShingJan <yuanj.shi@hotmail.com>
2016-05-20 23:46:54 -0700

    update of description on Findltineraty

--- Leetcode/src/findItinerary.java
+++ Leetcode/src/findItinerary.java
@@ -1,3 +1,16 @@
+/*
+After one trip, Lee got several tickets with departures and destinations
+His departure airport is JFK and he wants to know exactly his ltinerary
+This is at least one solution to every set of data
+When there is multiple answers, show the least one
+Example 1:
+tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
+Example 2:
+tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
+*/
+
 public class findItinerary {
     public List<String> findItinerary(String[][] tickets) {
         Map<String, PriorityQueue<String>> hashmap = new HashMap<String, PriorityQueue<String>>();
