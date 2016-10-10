70ed283ac3cfdde4284085dbc91c4a2b2e9d21d7
ShingJan <yuanj.shi@hotmail.com>
2016-10-11 01:37:26 +0800

    little update

--- python code/d&c.py
+++ python code/d&c.py
@@ -1,3 +1,4 @@
+
 def findSecretLocation(x1,x2,y1,y2):
     print(x1,x2,y1,y2)
     if test(x1,x2,y1,y2):
@@ -11,9 +12,9 @@ def findSecretLocation(x1,x2,y1,y2):
             findSecretLocation((x1+x2)//2+1, x2, y1,(y1+y2)//2)
             findSecretLocation((x1+x2)//2+1, x2, (y1+y2)//2+1, y2)
 
-def test(a1,a2,b1,b2):
-    if a1 <= 3 and 3 <= a2:
-        if b1 <= 5 and 5 <= b2:
+def test(a1, a2, b1, b2):
+    if a1 <= 3 <= a2:
+        if b1 <= 5 <= b2:
             return True;
     else:
         return False;
