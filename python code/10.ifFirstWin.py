48f51775fbc089e8d9e252867ffe5cf855b73e46
ShingJan <yuanj.shi@hotmail.com>
2016-11-05 23:47:44 +0800

    not finished yet

--- python code/10.ifFirstWin.py
+++ python code/10.ifFirstWin.py
@@ -1,20 +1,17 @@
 def firstWin(x):
     n = len(x)
-    largest = [[0 for i in range(n)] for j in range(n)]
-    sum = [[0 for i in range(n)] for j in range(n)]
+    largest = [0 for i in range(n)]
+    sum = [0 for i in range(n)]
 
-    for i in range(n):
-        sum[i][i] = x[i]
-
-    for i in range(n):
-        for j in range(n):
-            if i < j:
-                sum[i][j] = sum[i][j-1] + x[j]
+    for i in range(1,n):
+        sum[i] = sum[i-1] + x[i-1]
 
     for i in range(n):
         largest[i] = x[i]
 
-    for i in range(n-1):
+    temp = 0
+    for i in range(1,n):
+        temp = largest[n-i]
         for j in range(n-1):
             largest[i][j] = max(x[j] + sum[i][j-1] - largest[i][j-1], x[i] + sum[i+1][j] - largest[i + 1][j])
 
