37d18a458ffbf6b3b1fa06f90357d29512d888e5
ShingJan <yuanj.shi@hotmail.com>
2017-01-07 00:02:59 +0800

    find median of two sorted arrays in o(m+n) time problem update

---
+++ JavaCode/src/com/example/medianTwoArrays.java
@@ -0,0 +1,25 @@
+package com.example;
+
+/**
+ * Created by Isaac on 1/6/17.
+ * Median of Two Sorted Arrays
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * The median is (2 + 3)/2 = 2.5
+ */
+public class medianTwoArrays {
+    public static void main(String [] args){
+        float[] num1 = {1,2};
+        float[] num2 = {3,4};
+        System.out.println(findMedian(num1, num2));
+    }
+
+    public static float findMedian(float[] num1, float[] num2){
+        float result = 0;
+
+
+
+
+        return result;
+    }
+}
