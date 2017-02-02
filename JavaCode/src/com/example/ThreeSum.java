0a19ab84e22242a8164bdb7206f3cd0f7228737d
ShingJan <yuanj.shi@hotmail.com>
2017-02-02 23:58:12 +0800

    three sum problem update

---
+++ JavaCode/src/com/example/ThreeSum.java
@@ -0,0 +1,13 @@
+package com.example;
+import java.util.List;
+/**
+ * Created by Isaac on 2/2/17.
+ * Given an array S of n integers, are there elements a, b, c
+ * in S such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+    }
+}
